daaff417710b38abeb6f4051af1919bef9c97b7b
DucQuocXa <duc.ngogia310302@gmail.com>
2022-02-05 23:08:49 +0200

    chapter 3 database and JPA

--- BookStore/src/main/java/com/example/demo/BookStoreApplication.java
+++ BookStore/src/main/java/com/example/demo/BookStoreApplication.java
@@ -1,13 +1,38 @@
 package com.example.demo;
 
+import com.example.demo.domain.Book;
+import com.example.demo.domain.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+
+			Book bk1 = new Book("ReactJs", "Duc Ngo", 2020, "1234567", 20);
+			Book bk2 = new Book("ReactTs", "Duc Ngo", 2022, "1234567", 21);
+			Book bk3 = new Book("JavaScript", "Duc Ngo", 2021, "1234567", 22);
+			Book bk4 = new Book("NodeJs", "Duc Ngo", 2022, "1234567", 23);
+
+			repository.save(bk1);
+			repository.save(bk2);
+			repository.save(bk3);
+			repository.save(bk4);
+
+		};
+
 	}
 
 }
--- BookStore/src/main/java/com/example/demo/web/BookController.java
+++ BookStore/src/main/java/com/example/demo/web/BookController.java
@@ -1,12 +1,43 @@
 package com.example.demo.web;
 
+import com.example.demo.domain.Book;
+import com.example.demo.domain.BookRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookController {
     @GetMapping("/index")
     public String helloIndex() {
         return "index";
+    }
+
+    @Autowired
+    BookRepository repository;
+
+    @GetMapping("/booklist")
+    public String bookStoreFront(Model model) {
+
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
+
+    @PostMapping("/save")
+    public String saveBook(Book book) {
+        repository.save(book);
+        return "redirect:booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id, Model model) {
+        repository.deleteById(id);
+        return "redirect:../booklist";
     }
 }
